eaf06c53d4fcc954e604abc4fc5351f1506d7c87
GabrielGCSOARES <126501271+GabrielGCSOARES@users.noreply.github.com>
2026-02-24 19:04:27 -0300

    patch 0.2

--- frontend/main.py
+++ frontend/main.py
@@ -2,10 +2,15 @@ import pygame
 import sys
 
 pygame.init()
+pygame.mixer.init()
 
 WIDTH, HEIGHT = 600, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Jogo Elementar - Pygame")
+
+pygame.mixer.music.load("assets/musicatema.wav")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1) #vai rodar infinitamente
 
 WHITE = (255, 255, 255)
 BLUE = (50, 100, 255)
@@ -16,9 +21,10 @@ player_y = HEIGHT // 2
 speed = 5
 
 clock = pygame.time.Clock()
+FPS = 120
 
 while True:
-    clock.tick(60)
+    clock.tick(FPS)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
